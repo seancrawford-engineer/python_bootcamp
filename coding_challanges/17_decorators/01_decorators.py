76706d749026c4d8c4a4794c7480b1dc9d9963b7
Sean Crawford <66864745+seancrawford-engineer@users.noreply.github.com>
2021-01-21 16:21:58 -0800

    Add files via upload

---
+++ coding_challanges/17_decorators/01_decorators.py
@@ -0,0 +1,30 @@
+# ---- Do not change the code below ----
+# User identity dictionary
+user = {
+    'id': 1,
+    'name': 'jose',
+    'role': 'admin'
+}
+
+# delete_database() function, DO NOT CHANGE
+def delete_database():
+    # perform deletion
+    print('Database deleted!')
+
+# ---- Do not change the code above ----
+
+
+# You code starts here:
+# Define a check_permission() decorator:
+def check_permission(func):
+    def wrapper():
+        if user.get('role')=='admin':
+            return func()
+        else:
+            raise PermissionError('You are not an admin.')
+    return wrapper
+        
+ 
+secure_delete_database = check_permission(delete_database)
+
+# Use the check_permission() decorator and delete_database() function to create a secure_delete_database() function:
